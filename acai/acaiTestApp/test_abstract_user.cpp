0d65a65bc4b5088644caa860a3c7fc6c936d7a00
Andrew Starritt <starritt@netspace.net.au>
2017-06-09 22:50:52 +1000

    Added additonal test cases for client sets and abstract user.

---
+++ acai/acaiTestApp/test_abstract_user.cpp
@@ -0,0 +1,140 @@
+// $File: //depot/sw/epics/acai/acaiTestApp/test_abstract_user.cpp $
+// $Revision: #1 $
+// $DateTime: 2017/04/18 22:56:57 $
+// Last checked in by: $Author: andrew $
+//
+
+#include <iostream>
+#include <acai_client_types.h>
+#include <acai_client.h>
+#include <acai_abstract_client_user.h>
+#include <epicsThread.h>
+
+//==============================================================================
+//
+//==============================================================================
+//
+class ClientUser : public ACAI::Abstract_Client_User {
+public:
+   explicit ClientUser ();
+   ~ClientUser ();
+
+protected:
+   void connectionUpdate (ACAI::Client* sender,
+                          const bool isConnected);
+};
+
+//------------------------------------------------------------------------------
+//
+ClientUser::ClientUser () : ACAI::Abstract_Client_User ()
+{
+   std::cout << "constucted client user\n";
+}
+
+//------------------------------------------------------------------------------
+//
+ClientUser::~ClientUser ()
+{
+   std::cout << "destucted client user\n";
+}
+
+
+//------------------------------------------------------------------------------
+//
+void ClientUser::connectionUpdate (ACAI::Client* sender,
+                                   const bool isConnected)
+{
+   std::cout << "client user.connectionUpdate "
+             << sender->pvName()
+             << (isConnected ? " connected" : " disconnected")
+             << "\n";
+}
+
+
+//==============================================================================
+//
+//==============================================================================
+//
+class TestClient : public ACAI::Client {
+public:
+   explicit TestClient (const ACAI::ClientString& pvName);
+   ~TestClient ();
+
+protected:
+   void connectionUpdate (const bool isConnected);
+
+};
+
+//------------------------------------------------------------------------------
+//
+TestClient::TestClient (const ACAI::ClientString& pvName) :
+   ACAI::Client (pvName)
+{
+   std::cout << "constucted test client " << pvName << "\n";
+}
+
+//------------------------------------------------------------------------------
+//
+TestClient::~TestClient ()
+{
+   std::cout << "destructed test client " << this->pvName() << " \n";
+}
+
+//------------------------------------------------------------------------------
+//
+void TestClient::connectionUpdate (const bool isConnected)
+{
+   std::cout << "test client connectionUpdate "
+             << this->pvName()
+             << (isConnected ? " connected" : " disconnected")
+             << "\n";
+}
+
+
+//==============================================================================
+//
+int main () {
+   std::cout << "test abstract client user starting ("
+             << ACAI_VERSION_STRING << ")\n\n";
+
+   ACAI::Client::initialise ();
+
+   ClientUser* user = new ClientUser ();
+
+   TestClient* t1 = new TestClient ("T1");
+   TestClient* t2 = new TestClient ("T2");
+   TestClient* t3 = new TestClient ("T3");
+   TestClient* t4 = new TestClient ("T4");
+
+   user->registerClient (t1);
+   user->registerClient (t2);
+   user->registerClient (t3);
+   std::cout << "\n";
+
+   std::cout << "open registered clients\n";
+   user->openRegisteredChannels();
+   std::cout << "registered clients opened\n";
+
+   std::cout << "open T4 client\n";
+   t4->openChannel();
+   std::cout << "T4 client opened\n";
+
+   bool ok = user->waitAllRegisteredChannelsReady (2.0, 0.1);
+   std::cout << "all channels open " << (ok ? "yes" : "no") <<  "\n";
+
+   for (int t = 0; t < 500; t++) {
+      epicsThreadSleep (0.02);   // 20mSec
+      ACAI::Client::poll ();     // call back function invoked from here
+   }
+
+   std::cout << "close registered clients\n";
+   user->closeRegisteredChannels();
+
+   delete user;
+
+   ACAI::Client::finalise();
+   std::cout << "\ntest abstract client user complete\n";
+   return 0;
+}
+
+// end
---
+++ acai/acaiTestApp/test_client_set.cpp
@@ -0,0 +1,103 @@
+// $File: //depot/sw/epics/acai/acaiTestApp/test_client_set.cpp $
+// $Revision: #1 $
+// $DateTime: 2017/04/18 14:57:49 $
+// Last checked in by: $Author: andrew $
+//
+
+#include <iostream>
+#include <acai_client_types.h>
+#include <acai_client.h>
+#include <acai_client_set.h>
+
+
+class TestClient : public ACAI::Client {
+public:
+   explicit TestClient (const ACAI::ClientString& pvName) :
+      ACAI::Client (pvName)
+   {
+      std::cout << "constucted test client " << pvName << "\n";
+   }
+
+   ~TestClient ()
+   {
+      std::cout << "destructed test client " << this->pvName() << " \n";
+   }
+};
+
+//------------------------------------------------------------------------------
+//
+static void dump (ACAI::Client* client, void* context)
+{
+   const char* message =  context ? (const char *) context : "nil";
+   std::cout << "dump client (" << message << ") " << client->pvName() << "\n";
+}
+
+#define DUMP_SET(sn,expect) {                                         \
+   std::cout << #sn << " iteration - expect " << expect << "\n";      \
+   sn->iterateChannels (dump, (char*)(#sn));                          \
+   std::cout << "count:    " << sn->count () << "\n";                 \
+   std::cout << "contains: " << (sn->contains (t1) ? 1 : 0);          \
+   std::cout << ", "         << (sn->contains (t2) ? 2 : 0);          \
+   std::cout << ", "         << (sn->contains (t3) ? 3 : 0);          \
+   std::cout << ", "         << (sn->contains (t4) ? 4 : 0);          \
+   std::cout << ", "         << (sn->contains (t5) ? 5 : 0);          \
+   std::cout << ", "         << (sn->contains (t6) ? 6 : 0);          \
+   std::cout << "\n\n";                                               \
+}
+
+
+//------------------------------------------------------------------------------
+//
+int main () {
+   std::cout << "test client set starting ("
+             << ACAI_VERSION_STRING << ")\n\n";
+
+   TestClient* t1 = new TestClient ("T1");
+   TestClient* t2 = new TestClient ("T2");
+   TestClient* t3 = new TestClient ("T3");
+   TestClient* t4 = new TestClient ("T4");
+   TestClient* t5 = new TestClient ("T5");
+   TestClient* t6 = new TestClient ("T6");
+   std::cout << "\n";
+
+   ACAI::Client_Set* s1 = new ACAI::Client_Set ();
+   ACAI::Client_Set* s2 = new ACAI::Client_Set (true);
+
+   s1->insert (t1);
+   s1->insert (t2);
+   s1->insert (t3);
+   s1->insert (t3);
+   s1->insert (t3);
+
+   s2->insert (t3);
+   s2->insert (t4);
+   s2->insert (t5);
+   s2->insert (t6);
+
+   DUMP_SET (s1, "T1,T2,T3");
+   s1->remove (t2);
+   DUMP_SET (s1, "T1,T3");
+
+   DUMP_SET (s2, "T3,T4,T5,T6");
+   s2->insertAllClients (s1);
+
+   DUMP_SET (s1, "T1,T3");
+   DUMP_SET (s2, "T1,T3,T4,T5,T6");
+
+   std::cout << "clear set1\n";
+   s1->clear();
+   DUMP_SET (s1, "none");
+
+   std::cout << "deleting set 1\n";
+   delete s1;
+   std::cout << "set 1 deleted\n";
+
+   std::cout << "\ndeleting set 2\n";
+   delete s2;
+   std::cout << "set 2 delete\n";
+
+   std::cout << "\ntest client set complete\n";
+   return 0;
+}
+
+// end
